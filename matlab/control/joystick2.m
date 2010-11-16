81889384e0f451f17165bbcb3fd93c1bb851ea4f
unknown <dcsl@.(none)>
2010-11-16 15:40:17 -0500

    Blake's modifications to joystick2 from Sun Nov 14

--- matlab/control/joystick2.m
+++ matlab/control/joystick2.m
@@ -5,7 +5,7 @@ upcmd = zeros(1,2);
 fwdcmd = zeros(1,2);
 servocmd = zeros(1);
 
-spd = 10; vrt = 10; rev = 10; max = 40;     % set the default speeds
+spd = 10; vrt = 15; rev = 20; max = 40;     % set the default speeds
 dead1 = 0.00;   % set the size of the deadzone for the joystick (fwd/back)
 dead2 = 0.05;   % set the size of the deadzone for the joystick (left/right)
 
@@ -14,29 +14,33 @@ forward = 1;
 while 1
      [axes, buttons, povs] = read(joy);
      
+     % throttle max horizontal velocity with dial
+     % max = 20*abs(axes(3)-1);
+     
      % the following section is for speed adjustment with button control
-%     if (buttons(3)==1 && buttons(2) == 0 && spd < 40), 
-%         spd = spd + 5; end
-%     if (buttons(3)==0 && buttons(2) == 1 && spd > 0), 
-%         spd = spd - 5; end
+    if (buttons(6)== 1 && buttons(7) == 0 && vrt < 40), 
+        vrt = vrt + 5; end
+    if (buttons(6)== 0 && buttons(7) == 1 && vrt > 0), 
+        vrt = vrt - 5; end
      
      % vertical control
-     if (buttons(1)), upcmd(1) = 1; upcmd(2) = vrt; 
-     else if (buttons(3)), upcmp(1) = 0; upcmd(2) = vrt;
+     if (buttons(1)), upcmd(1) = 0; upcmd(2) = vrt; 
+     else if (buttons(3)), upcmd(1) = 1; upcmd(2) = vrt;
          else upcmd(2) = 0;
          end
      end
      
      % forward/reverse control
-     fwdcmd(1) = (axes(2) > 0);
-     if (abs(axes(2)) < dead), fwdcmd(2) = 0;
-     else if (axes(2) < 0), fwdcmd(2) = rev;
-         else fwdcmd(2) = floor((abs(axes(2))-dead)*max/(1-dead));
+     fwdcmd(1) = (axes(2) < 0);
+     if (abs(axes(2)) < dead1), fwdcmd(2) = 0;
+     else if (axes(2) > 0 && abs(axes(2)-dead1)/(1-dead1) > rev /max), 
+             fwdcmd(2) = rev;
+         else fwdcmd(2) = floor((abs(axes(2))-dead1)*max/(1-dead1));
          end
      end
      
      % servo control
-     servocmd = 68-floor(32*axes(1));
+     servocmd = 68-floor(40*axes(1));
      
      up(upcmd(1),upcmd(2),s);
      pause(.1)
