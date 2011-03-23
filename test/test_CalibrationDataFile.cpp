580d342959ba4ba1eb9b91b9f0fd523c8e1ad84f
Dan Swain <dswain@princeton.edu>
2011-03-23 13:03:01 -0400

    Added tests for calibration data file writing.

--- test/test_CalibrationDataFile.cpp
+++ test/test_CalibrationDataFile.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char** argv)
 {
     Beluga_CalibrationDataFile testfile("../matlab/calibration/test.dat");
+    Beluga_CalibrationDataFile testOutFile("testOut.dat", CALIB_WRITE);
 
     if(!testfile.didLoadOK())
     {
@@ -16,6 +17,27 @@ int main(int argc, char** argv)
     testfile.getCalibration(&d);
     
     std::cout << calibrationDataToString(d);
+
+    if(!testOutFile.didLoadOK())
+    {
+        std::cerr << "Failure to load output file\n";
+        return 1;
+    }
+
+    d.info = std::string("WELL HELLO");
+    testOutFile.writeCalibration(d);
+
+    Beluga_CalibrationDataFile testInFile("testOut.dat");
+    if(!testInFile.didLoadOK())
+    {
+        std::cerr << "Failure to load or parse second file\n";
+        return 1;
+    }
+
+    CalibrationData d2;
+    testInFile.getCalibration(&d2);
+
+    std::cout << calibrationDataToString(d2);
 
     return 0;
 }
