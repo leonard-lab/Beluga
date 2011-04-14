8e64350bb6e3670429ab8dacab3b75b56390dc68
Dan Swain <dswain@princeton.edu>
2011-04-13 21:17:06 -0400

    Rhubarbified the server.

--- IPC/beluga_server.rb
+++ IPC/beluga_server.rb
@@ -1,25 +1,4 @@
-#!/usr/bin/ruby
-
-require 'gserver'
-
-Server_Shutdown = "S"
-Server_ClientQuit = "Q"
-
-Server_Cmds = [
-               {:name => "get"},
-               {:name => "set"},
-               {:name => "ping"}
-              ]
-
-Cmd_Position = {:name =>"position", :setArgs =>3}
-Cmd_Cmd = {:name => "command", :setArgs =>3}
-Cmd_Verbose = {:name => "verbose", :setArgs => 1}
-
-Server_Cmds_GetIndexed = [Cmd_Position, Cmd_Cmd]
-Server_Cmds_SetIndexed = [Cmd_Position, Cmd_Cmd]
-
-Server_Cmds_Get = [Cmd_Verbose]
-Server_Cmds_Set = [Cmd_Verbose]
+require 'rhubarb/rhubarb.rb'
 
 class Robot
 
@@ -39,7 +18,7 @@ class Robot
 
 end
 
-class BelugaServer < GServer
+class BelugaServer < Rhubarb
   
   def initialize(*args)
     super(*args)
@@ -51,13 +30,15 @@ class BelugaServer < GServer
     @@robots << Robot.new(1);
     @@robots << Robot.new(2);
     @@robots << Robot.new(3);
-    @@index_max = 3;
 
-    @@verbose = false
+    cmd_position = {:name => "position", :setArgs => 3, :maxIndex => 3}
+    cmd_cmd = {:name => "command", :setArgs => 3, :maxIndex => 3}
 
-    @@client_id = 0
-    @@chat = []
-    
+    @indexed_get_commands << cmd_position
+    @indexed_get_commands << cmd_cmd
+    @indexed_set_commands << cmd_position
+    @indexed_set_commands << cmd_cmd
+
   end
 
   def robot(id)
@@ -85,174 +66,15 @@ class BelugaServer < GServer
     @@robots[i].position_z = args[2].to_f
     getPosition(i)
   end
-
-  def getVerbose(args, cmd_def)
-    @@verbose ? "on" : "off"
-  end
-
-  def setVerbose(args, cmd_def)
-    if args[2] == "on"
-      @@verbose = true
-    end
-    if args[2] == "off"
-      @@verbose = false
-    end
-    getVerbose(args, cmd_def)
-  end
-
-  def respondToIndexedOp(words, op, resp_def)
-
-    return unknownCommand({:message => "Not Enough Arguments"}) unless words.size >= 3
-
-    opwords = words[2..words.size]
-    responder = op + resp_def[:name].capitalize
-    
-    if respond_to?(responder)
-      if op == "set"
-        nargs = resp_def[:setArgs]
-        return "Indexing error" unless opwords.size % (nargs + 1) == 0
-        response = ""
-        (0..opwords.size-nargs-1).step(nargs+1).each{|w|
-          if opwords[w].to_i.to_s == opwords[w] && (0..@@index_max).include?(opwords[w].to_i)
-            response += send(responder, opwords[w].to_i, opwords[w+1..w+nargs])
-          end
-        }
-        response = "Indexing error" if response.strip.empty?
-        return response
-      elsif op == "get"
-        do_all = opwords.include?("*")
-        response = ""
-        (0..@@index_max).each{|i|
-          response += send(responder, i) if opwords.include?(i.to_s) or do_all
-        }
-        response = "Indexing error" if response.strip.empty?
-        return response
-      else
-        unkownCommand({:message => "Unknown indexed command"})
-      end
-    else
-      unknownCommand({:message => "No responder"})
-    end
-
-    unknownCommand({:message => "No responder present"})
-
-  rescue
-    unknownCommand({:message => "Responding to indexed command"})
-  end
-
-  def respondToSet(words)
-    resp_def = Server_Cmds_SetIndexed.detect{|c| c[:name] == words[1]}
-
-    if resp_def
-      respondToIndexedOp(words, "set", resp_def)
-    else
-      resp_def = Server_Cmds_Set.detect{|c| c[:name] == words[1]}
-      if resp_def && respond_to?("set" + words[1].capitalize)
-        send("set" + words[1].capitalize, words, resp_def)
-      else
-        unknownCommand({:message => "Unknown Set command"})
-      end
-    end
-
-  rescue unknownCommand({:message => "Responding to Set command"})
-      
-  end
-
   
-  def respondToGet(words)
-    resp_def = Server_Cmds_GetIndexed.detect{|c| c[:name] == words[1]}
-
-    if resp_def
-      respondToIndexedOp(words, "get", resp_def)
-    else
-      resp_def = Server_Cmds_Get.detect{|c| c[:name] == words[1]}
-      if resp_def && respond_to?("get" + words[1].capitalize)
-        send("get" + words[1].capitalize, words, resp_def)
-      else
-        unknownCommand({:message => "Unknown Get command"})
-      end
-    end
-
-  rescue unknownCommand({:message => "Responding to Get command"})
-      
-  end
-
   def respondToPing(words)
     "PONG!"
   end
 
-  def unknownCommand(def_info = {})
-    info = {
-      :method_name => "",
-      :message => "",
-      :self_message => ""
-    }.merge def_info
-
-    resp = "ERR " + self.class.to_s
-    resp += " in method " + info[:method_name] unless info[:method_name].empty?
-    resp += ": " + info[:message] unless info[:message].empty?
-
-    in_resp = Time.now.strftime("[%a %b %e %R:%S %Y] ") + self.class.to_s
-    in_resp += " " +  self.host.to_s + ":" + self.port.to_s + " ERR "
-    in_resp += " in method " + info[:method_name] unless info[:method_name].empty?
-    in_resp += ": " + info[:message] unless info[:message].empty?    
-    in_resp += " + " + info[:self_message] unless info[:self_message].empty?
-    puts in_resp
-
-    resp
-    
+  def welcomeMessage(args)
+    "Welcome to BelugaServer, client #{@@client_id}."
   end
-  
-  def serve(io)
-    # Increment the client ID so each client gets a unique ID
-    @@client_id += 1
-    my_client_id = @@client_id
 
-    io.puts("Hello from BelugaServer.  You are client #{@@client_id}.  I have 4 Robots.")
-
-    do_shutdown = false
-    
-    loop do
-
-      begin
-        line = io.readline.strip
-
-        puts "GOT:  |" + line + "|\n" unless !@@verbose
-
-        if line == Server_ClientQuit
-          io.puts "Goodbye"
-          break
-        end
-
-        if line == Server_Shutdown
-          io.puts "Goodbye"
-          do_shutdown = true
-          break
-        end
-
-        line = line.downcase
-        words = line.split(" ")
-
-        cmd_def = Server_Cmds.detect{|c| c[:name] == words[0]}
-        responder = "respondTo" + words[0].capitalize
-
-        if respond_to?(responder)
-          io.puts send(responder, words).strip
-        else
-          io.puts unknownCommand({:message => "Unknown primary command"})
-        end
-
-      rescue
-        io.puts "Server error.  Try again."
-      end
-      
-    end
-
-    if do_shutdown
-      self.stop
-    end
-    
-   end
 end
 
 server = BelugaServer.new(1234, '127.0.0.1')
