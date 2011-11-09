65a74fb665c5528d9daaee620ad7cdf2f95e9bea
Dan Swain <dswain@princeton.edu>
2011-11-09 12:21:00 -0500

    Add some error handling in belugaGetPositionIPC

--- IPC/matlab/belugaGetPositionIPC.m
+++ IPC/matlab/belugaGetPositionIPC.m
@@ -4,10 +4,17 @@ if nargin == 1,
     sock = [];
 end
 
-resp = belugaIPCMessage(sprintf('get position %d', robot_id), sock);
+resp_string = belugaIPCMessage(sprintf('get position %d', robot_id), sock);
 
-resp = sscanf(resp, '%d %f %f %f');
+resp = sscanf(resp_string, '%d %f %f %f');
 
-x = resp(2);
-y = resp(3);
-z = resp(4);
+if(length(resp) < 3)
+    fprintf('Unexpected response from server: %s', resp_string)
+    x = -1;
+    y = -1;
+    z = -1;
+else
+    x = resp(2);
+    y = resp(3);
+    z = resp(4);
+end
