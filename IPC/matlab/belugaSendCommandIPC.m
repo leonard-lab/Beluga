0cf858ab338157275944f1d2f5f8a7dded2eac46
Dan Swain <dswain@princeton.edu>
2011-11-10 16:02:08 -0500

    Delete old get/set command files.

--- IPC/matlab/belugaSendCommandIPC.m
+++
@@ -1,7 +0,0 @@
-function belugaSendCommandIPC(robot_id, go_x, go_y, go_z, sock)
-
-if(nargin == 4)
-    sock = [];
-end
-
-belugaIPCMessage(sprintf('set command %d %f %f %f', robot_id, go_x, go_y, go_z), sock);
