dd29e273aa193ad2dab58714dec01712eee7b3d8
Dan Swain <dswain@princeton.edu>
2011-11-10 18:02:00 -0500

    Made stopBelugaSimulator more robust

--- IPC/simulator/stopBelugaSimulator.m
+++ IPC/simulator/stopBelugaSimulator.m
@@ -1,7 +1,13 @@
-function stopBelugaSimulator()
+function stopBelugaSimulator(sock)
 
 t = timerfind('name', 'BelugaSimTimer');
 if ~isempty(t)
     stop(t);
     delete(t);
 end
+
+if nargin > 0 && ~isempty(sock)
+    fclose(sock);
+end
+
+stopBelugaServer
