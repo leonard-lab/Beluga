088bfdac9b8da7ebba098f43a6c770d72e328455
Dan Swain <dswain@princeton.edu>
2011-11-10 10:49:39 -0500

    Add startBelugaServer and isBelugaServerRunning

---
+++ IPC/matlab/startBelugaServer.m
@@ -0,0 +1,50 @@
+function success = startBelugaServer(logfile)
+%
+% success = startBelugaServer(logfile)
+%
+% Starts the beluga server, checks for a connection, and returns 1
+% on success.  On error, throws an error (and returns 1).
+%
+% Checks to make sure the server isn't already running.
+
+if nargin == 0,
+    logfile = 'beluga_server.log';
+end
+
+pid = isBelugaServerRunning();
+if pid > 0,
+    error('Beluga IPC server is already running as process with PID %s', pid)
+    success = 0;
+    return
+end
+
+server_path = fullfile(fileparts(mfilename('fullpath')), '../beluga_server.rb');
+
+cmd = '';
+if ispc
+    cmd = sprintf('start "server" "ruby %s >> %s 2>&1"', server_path, logfile);
+else
+    cmd = sprintf('ruby "%s" >> "%s" 2>&1 &', server_path, logfile);
+end
+
+[status, r] = system(cmd);
+if status > 0,
+    error(r)
+    success = 0;
+    return
+end
+
+disp('Server launched, checking connection...')
+pause(1)
+
+sock = getBelugaIPCSocket('127.0.0.1', 1234);
+resp = belugaIPCMessage('ping', sock);
+
+if ~strcmp(resp, 'PONG!')
+    error('Failure to confirm server launch.')
+    success = 0;
+    return
+end
+
+disp('Success.')
+success = 1;
