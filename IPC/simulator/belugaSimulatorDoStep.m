1bb6330964697bb4c3ca91fe04cfd6376962c455
Paul Reverdy <preverdy@princeton.edu>
2011-11-18 14:50:38 -0500

    First version with feedback linearization controller. Gains need to be tuned for better speed.

--- IPC/simulator/belugaSimulatorDoStep.m
+++ IPC/simulator/belugaSimulatorDoStep.m
@@ -1,58 +1,90 @@
 function [X_o, Y_o, Z_o] = belugaSimulatorDoStep(GO_X, GO_Y, GO_Z)
 
-persistent X;
+% Function [X_o, Y_o, Z_o] = belugaSimulatorDoStep(GO_X, GO_Y, GO_Z)
+%
+% Function to simulate the closed loop dynamics of the system of 4 vehicles
+%
+% All inputs and outputs are 4 x 1 vectors; GO_i are waypoint coordinates
+% for the four vehicles, while X_o, Y_o, Z_o are current vehicle locations.
+
+% define persistent vehicle state variables
+persistent X;       % coordinates
 persistent Y;
 persistent Z;
 persistent TH;
-persistent V;
+persistent V1;      % velocities; V2 is assumed to be driven to 0 by low-level control
+persistent V3;
+persistent OMEGA;
 
 if isempty(X),
     X = zeros(4, 1);
     Y = zeros(4, 1);
     Z = zeros(4, 1);
     TH = zeros(4, 1);
-    V = 0.05*ones(4, 1);
+    V1 = 0.05*ones(4, 1);
+    V3 = zeros(4, 1);
+    OMEGA = zeros(4, 1);
 end
 
-rmax = 3.2;
-zmax = 2.3;
+X_o = zeros(4,1);       % initialize output variables
+Y_o = zeros(4,1);
+Z_o = zeros(4,1);
 
-kth = -0.1;
-kz = 0.1;
+rmax = 3.0;     % tank size variables (m)
+zmax = 2.286;
 
-vmax = 0.05;
-dr1 = 0.5;
+% extract the sampling rate of the simulation from the timer
+t = timerfind('name','BelugaSimTimer');
+dt = get(t,'Period');
+clear t;
+
+vmax = 1;       % maximum forward speed (m/s)
+
+% control gains
+kPHoriz = 0.5;  % proportional control for error -> xDot, yDot
+kFbkLin = 1;    % feedback linearization gain
+kz = 0.1;       % vertical controller proportional gain
 
 for ix = 1 : 4,
     x = X(ix);
     y = Y(ix);
     z = Z(ix);
     th = TH(ix);
-    v = V(ix);
+    v1 = V1(ix);
+    v3 = V3(ix);
+    omega = OMEGA(ix);
     
     go_x = GO_X(ix);
     go_y = GO_Y(ix);
     go_z = GO_Z(ix);
     
-    dx = go_x - x;
+    R = [cos(th) -sin(th);  % compute the rotation matrix for future use
+         sin(th)  cos(th)];
+    
+    % Controls
+    dx = go_x - x;  % location error
     dy = go_y - y;
     dz = go_z - z;
+
+    linVels = kPHoriz*[dx dy];  % proportional control for linear velocities
     
-    dr = sqrt(dx^2 + dy^2);
+    bodyVels = kFbkLin*linVels*R;   % apply feedback linearization
     
-    th = th + kth*sin(th - atan2(dy,dx));
+    v1 = bodyVels(1); omega = bodyVels(2);   % pull out the velocities
     
-    if(dr > dr1)
-        v = vmax;
-    else
-        v = vmax*(dr/dr1);
+    if(v1 > vmax)   % saturate forward velocity
+        v1 = vmax;
     end
     
-    x = x + v*cos(th);
-    y = y + v*sin(th);
+    % integrate the kinematics
+    th = th + dt*omega;
+    x = x + v1*cos(th);
+    y = y + v1*sin(th);
     
+    % apply proportional control to the z dof
     z = z + kz*dz;
-    
+
+    % Enforce the 'stay in the tank' constraints
     r = sqrt(x.^2 + y.^2);
     if r > rmax,
         % this is not an exact collision correction
@@ -60,7 +92,7 @@ for ix = 1 : 4,
         rnew = rmax - (r - rmax);
         x = rnew*cos(phi);
         y = rnew*sin(phi);
-        v_vec = [v*cos(th); v*sin(th)];
+        v_vec = [v1*cos(th); v1*sin(th)];
         v_radial = [cos(phi) sin(phi)]*v_vec;
         v_perp = [-sin(phi) cos(phi)]*v_vec;
         v_vec_new = -v_radial*[cos(phi); sin(phi)] + v_perp*[-sin(phi); cos(phi)];
@@ -78,7 +110,9 @@ for ix = 1 : 4,
     Y(ix) = y;
     Z(ix) = z;
     TH(ix) = th;
-    V(ix) = v;
+    V1(ix) = v1;
+    V3(ix) = v3;
+    OMEGA(ix) = omega;
     
     X_o(ix, 1) = x;
     Y_o(ix, 1) = y;
