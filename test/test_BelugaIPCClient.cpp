08890944d07aebae64cf7ae8362dd2b22abfe47b
Dan Swain <dswain@princeton.edu>
2012-04-16 14:25:41 -0400

    Refactor some test code into BelugaTest.h

--- test/test_BelugaIPCClient.cpp
+++ test/test_BelugaIPCClient.cpp
@@ -1,16 +1,6 @@
+#include "BelugaTest.h"
+
 #include "BelugaIPCClient.h"
-
-const int ERROR = -1;
-const int OK = 0;
-
-#define START_TEST(x) std::cout << x << "... ";
-#define RETURN_ERROR(x) { std::cerr << x << std::endl; return ERROR; }
-#define RETURN_ERROR_ELSE_OK(x) RETURN_ERROR(x) else { std::cout << "OK" << std::endl; }
-
-#define DO_TEST(desc, cond, err_msg) \
-    START_TEST(desc); \
-    if(cond) RETURN_ERROR_ELSE_OK(err_msg);
-
 
 enum OP
 {
@@ -19,81 +9,6 @@ enum OP
     GET_CONTROLS,
     SET_CONTROLS
 };
-
-double randomfloat()
-{
-
-    double f;
-#ifdef _WIN32
-    f = ((rand() % 1000) / 1000.0);
-#else  
-    f = ((rand() % 1000000) / 1000000.0);
-#endif
-    return 3*(f - 0.5);
-}
-
-std::vector<double> randomVector(unsigned int size)
-{
-    std::vector<double> r(size);
-
-    for(unsigned int i = 0; i < size; i++)
-    {
-        r[i] = randomfloat();
-    }
-    return r;
-}
-
-bool eq_wf(double a, double b)
-{
-    char a_s[16];
-    char b_s[16];
-
-    sprintf(a_s, "%4.3f", a);
-    sprintf(b_s, "%4.3f", b);
-
-    return (strncmp(a_s, b_s, 16) == 0);
-}
-
-bool check_doubles_match(double a, double a_exp,
-                         double b, double b_exp,
-                         double c, double c_exp,
-                         const char* a_label,
-                         const char* b_label,
-                         const char* c_label,
-                         std::string* err_msg)
-{
-    std::ostringstream ss(*err_msg);
-    if(!eq_wf(a, a_exp) || !eq_wf(b, b_exp) || !eq_wf(c, c_exp))
-    {
-        ss << std::endl << "Mismatch error: "
-           << "\tExpected " << a_label << "to be " << a_exp << ", got " << a << std::endl
-           << "\tExpected " << b_label << "to be " << b_exp << ", got " << b << std::endl
-           << "\tExpected " << c_label << "to be " << c_exp << ", got " << c << std::endl;
-        *err_msg = ss.str();
-        return false;
-    }
-    return true;
-}
-
-bool check_vector_match(const std::vector<double>& a,
-                        const std::vector<double>& a_exp,
-                        const std::vector<double>& b,
-                        const std::vector<double>& b_exp,
-                        const std::vector<double>& c,
-                        const std::vector<double>& c_exp,
-                        const char* a_label,
-                        const char* b_label,
-                        const char* c_label,
-                        std::string* err_msg)
-{
-    bool r = true;
-    for(unsigned int i = 0; i < a.size(); i++)
-    {
-        r &= check_doubles_match(a[i], a_exp[i], b[i], b_exp[i], c[i], c_exp[i],
-                                 a_label, b_label, c_label, err_msg);
-    }
-    return r;
-}
 
 bool check_single(belugaIPCClient* client,
                            OP op,
--- test/test_Controller.cpp
+++ test/test_Controller.cpp
@@ -1,24 +1,34 @@
-#include <iostream>
+#include "BelugaTest.h"
 
 #include "BelugaControl.h"
 
-const int ERROR = -1;
-const int OK = 0;
-
-#define START_TEST(x) std::cout << x << "... ";
-#define RETURN_ERROR(x) { std::cerr << x << std::endl; return ERROR; }
-#define RETURN_ERROR_ELSE_OK(x) RETURN_ERROR(x) else { std::cout << "OK" << std::endl; }
-
-#define DO_TEST(desc, cond, err_msg) \
-    START_TEST(desc); \
-    if(cond) RETURN_ERROR_ELSE_OK(err_msg);
-
-#define RUN_TEST_SUITE(desc, tester) \
-    std::cout << "Starting tests for " << desc << "..." << std::endl; \
-    if(tester) return ERROR;
-
 int testBelugaLowLevelControlLaw()
 {
+    BelugaLowLevelControlLaw control_law;
+
+    std::string err_msg;
+
+    /* the output should have BELUGA_CONTROL_SIZE elements */
+    mt_dVector_t u_in, u_out, state;
+    u_in.resize(3, 0.0);
+    state.resize(4, 0.0);
+
+    START_TEST("Checking output vector size");
+    u_out = control_law.doControl(state, u_in);
+    if(u_out.size() != BELUGA_CONTROL_SIZE)
+        RETURN_ERROR_ELSE_OK("Incorrect size " << u_out.size());
+
+    /* the vertical thrust should be zero when u_vert is zero */
+    u_in = randomVector(3);
+    state = randomVector(4);
+    u_in[BELUGA_CONTROL_VERT_SPEED] = 0.0;
+    state[BELUGA_STATE_Z] = z_off;
+    
+    START_TEST("Checking that the vertical thrust is zero if no vertical command is given");
+    u_out = control_law.doControl(state, u_in);
+    if(u_out[BELUGA_CONTROL_VERT_SPEED] != 0.0)        
+        RETURN_ERROR_ELSE_OK("Control was " << u_out[BELUGA_CONTROL_VERT_SPEED]);
+    
     return OK;
 }
 
